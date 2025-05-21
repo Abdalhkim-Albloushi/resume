354f38291326874b85185ecbdbdebadbe2abf965
Abdalhkim-Albloushi <abdalhkim210598@gmail.com>
2025-05-22 00:33:25 +0400

    first commit

---
+++ src/app/projects/projects.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+@Component({
+  selector: 'app-projects',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './projects.component.html',
+  styleUrl: './projects.component.css',
+})
+export class ProjectsComponent {}
