9ef152fa678f6e4cfdd8255c5003eb44a361ef0e
Abdalhkim-Albloushi <abdalhkim210598@gmail.com>
2025-05-22 01:26:55 +0400

    lest change

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 import { APP_BASE_HREF } from '@angular/common';
+// import { CommonEngine } from '@angular/ssr';
 import express from 'express';
 import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
@@ -10,6 +11,8 @@ export function app(): express.Express {
   const serverDistFolder = dirname(fileURLToPath(import.meta.url));
   const browserDistFolder = resolve(serverDistFolder, '../browser');
   const indexHtml = join(serverDistFolder, 'index.server.html');
+
+  // const commonEngine = new CommonEngine();
 
   server.set('view engine', 'html');
   server.set('views', browserDistFolder);
@@ -37,7 +40,7 @@ export function app(): express.Express {
     //     publicPath: browserDistFolder,
     //     providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
     //   })
-
+    //   .then((html: any) => res.send(html))
     //   .catch((err: any) => next(err));
   });
 
